72c9c5981bae36d8fcf8a9f4b95c568d372208bd
stonehiy <stonehiy@gmail.com>
2019-11-07 16:15:47 +0800

    fix:提交Demo页面代码

--- jetpack_lib/src/main/java/io/github/stonehiy/lib/core/ViewModelEx.kt
+++ jetpack_lib/src/main/java/io/github/stonehiy/lib/core/ViewModelEx.kt
@@ -1,7 +1,6 @@
 package io.github.stonehiy.lib.core
 
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import io.github.stonehiy.lib.exception.ApiException
 import io.github.stonehiy.lib.exception.ServerException
 import io.github.stonehiy.lib.result.SResult
@@ -42,38 +41,6 @@ inline fun <T> ViewModel.coroutineJob(noinline block: suspend () -> IResult<T>, 
     }
 }
 
-/**
- * 开启协程（多任务）,还没有想好怎么做
- */
-@Deprecated("Deprecated")
-inline fun <T> ViewModel.coroutineMJob(vararg block: suspend () -> IResult<T>, liveData: CoreLiveData<T>) {
-    viewModelScope.launch {
-        withContext(Dispatchers.IO) {
-            Timber.i("coroutineJobScope: I'm working in thread ${Thread.currentThread().name}")
-            // Heavy work
-            liveData.postValue(SResult.Loading)
-//        delay(5000)
-            try {
-                block.forEach {
-                    val body = it()
-                    if (body.success()) {
-                        liveData.postValue(SResult.Success(body))
-                    } else {
-                        if (body.authentication401()) {
-                            liveData.postValue(SResult.Authentication401)
-                        } else {
-                            liveData.postValue(SResult.Error(ServerException.handleException(ApiException(body.errorMsg()))))
-                        }
-                    }
-                }
-            } catch (e: Exception) {
-                Timber.w(e)
-                liveData.postValue(SResult.Error(ServerException.handleException(e)))
-            }
-
-        }
-    }
-}
 
 
 
