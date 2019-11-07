93191867240541f740288356d91d9627d352ec2c
stonehiy <stonehiy@gmail.com>
2019-11-07 17:27:24 +0800

    fix:提交Demo页面代码

--- app/src/main/java/com/stonehiy/jetpackdemo/ApiSource.kt
+++ app/src/main/java/com/stonehiy/jetpackdemo/ApiSource.kt
@@ -6,41 +6,26 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
-/**
- * @author ShiGang <ShiGang, stonehiy@163.com>
- * @version v1.0
- * @Description TODO
- * @encoding UTF-8
- * @date 2019/5/21
- * @time 17:00
- * @修改记录 <pre>
- * 版本       修改人         修改时间         修改内容描述
- * --------------------------------------------------
- * <p>
- * --------------------------------------------------
- * </pre>
- */
-class ApiSource {
-    companion object {
-        private val okHttpClient =
-                OkHttpClient.Builder()
-                        .addInterceptor(HttpLoggingInterceptor().setLevel(
-                                if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY
-                                else HttpLoggingInterceptor.Level.NONE))
-                        .connectTimeout(15, TimeUnit.SECONDS)
-                        .readTimeout(15, TimeUnit.SECONDS)
-                        .writeTimeout(15, TimeUnit.SECONDS)
-                        .build()
-        @JvmStatic
-        val instance = Retrofit.Builder()
-                .baseUrl("https://wanandroid.com")
-                .addConverterFactory(GsonConverterFactory.create())
+
+object ApiSource {
+
+    private const val BASE_URL = "https://wanandroid.com"
+
+    private val okHttpClient =
+            OkHttpClient.Builder()
+                    .addInterceptor(HttpLoggingInterceptor().setLevel(
+                            if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY
+                            else HttpLoggingInterceptor.Level.NONE))
+                    .connectTimeout(15, TimeUnit.SECONDS)
+                    .readTimeout(15, TimeUnit.SECONDS)
+                    .writeTimeout(15, TimeUnit.SECONDS)
+                    .build()
+    val instance = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
 //                .addCallAdapterFactory(CoroutineCallAdapterFactory.invoke())
-                .client(okHttpClient)
-                .build().create(ApiService::class.java)
-
-
-    }
+            .client(okHttpClient)
+            .build().create(ApiService::class.java)!!
 
 
 }
